c71d8994a8a54391cc056063039e9cbef07d6fbd
Stefan Sietzen <stefan.sietzen@gmx.at>
2017-11-17 12:11:42 +0100

    reverted to old crossing count

--- model/page.py
+++ model/page.py
@@ -9,23 +9,18 @@ class Page(object):
         self.id = id
         self.graph = graph
         self.edges = set()
-        self.crossings = 0
-        self.crossings_valid = True
         
     def addEdge(self, edge):
         self.edges.add(edge)
-        self.crossings += len(edge.getCrossingSetForPage(self.id))
 
     def removeEdge(self, edge):
         self.edges.remove(edge)
-        self.crossings -= len(edge.getCrossingSetForPage(self.id))
 
     def numCrossings(self):
-#         doubledCrossings = 0
-#         for edge in self.edges:
-#             doubledCrossings += len(edge.getCrossingSetForPage(self.id))
-#       return doubledCrossings
-        return self.crossings
+        doubledCrossings = 0
+        for edge in self.edges:
+            doubledCrossings += len(edge.getCrossingSetForPage(self.id))
+        return doubledCrossings
 
     def getEdges(self):
         return self.edges
